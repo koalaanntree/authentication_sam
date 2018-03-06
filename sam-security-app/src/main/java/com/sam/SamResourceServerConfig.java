e310c5ed98cb6f15497918f0ece7ee3f1cfc747d
SamTheBetter <414706590@qq.com>
2018-03-06 10:42:06 +0800

    用户名密码登录完成

--- sam-security-app/src/main/java/com/sam/SamResourceServerConfig.java
+++ sam-security-app/src/main/java/com/sam/SamResourceServerConfig.java
@@ -1,7 +1,17 @@
 package com.sam;
 
+import com.sam.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.sam.properties.SecurityConstants;
+import com.sam.properties.SecurityProperties;
+import com.sam.validate.code.ValidateCodeSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 /**
  * @Author: huangxin
@@ -10,5 +20,55 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class SamResourceServerConfig {
+public class SamResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    protected AuthenticationSuccessHandler samAuthenticationSuccessHandler;
+
+    @Autowired
+    protected AuthenticationFailureHandler samAuthenticationFailureHandler;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer samSocialSecurityConfig;
+
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .successHandler(samAuthenticationSuccessHandler)
+                .failureHandler(samAuthenticationFailureHandler);
+
+        http.apply(validateCodeSecurityConfig)
+                .and()
+                .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                .apply(samSocialSecurityConfig)
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                        securityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*",
+                        securityProperties.getBrowser().getSignUpUrl(),
+                        securityProperties.getBrowser().getSession().getSessionInvalidUrl() + ".json",
+                        securityProperties.getBrowser().getSession().getSessionInvalidUrl() + ".html",
+                        securityProperties.getBrowser().getSignOutUrl(),
+                        "/user/regist")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable();
+    }
 }
