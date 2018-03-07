736329e828d81987c0f924bbfc80ccebc2cb1d1b
SamTheBetter <414706590@qq.com>
2018-03-07 16:48:45 +0800

    单点登录案例集成

---
+++ sso-server/src/main/java/com/sam/SSOServerApplication.java
@@ -0,0 +1,16 @@
+package com.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: huangxin
+ * @Date: Created in 下午4:30 2018/3/7
+ * @Description:
+ */
+@SpringBootApplication
+public class SSOServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SSOServerApplication.class, args);
+    }
+}
